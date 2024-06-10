4a0e17df507f69801da44319729acfc7b76a48e0
BarshaKashyap <barshakashyap5@gmail.com>
2024-06-10 23:38:55 +0530

    Literals and operators

---
+++ Ex_10062024/Lab027.py
@@ -0,0 +1,29 @@
+# Literals
+# var_name = variable_value
+name = "Barsha"
+print(name)
+print(type(name))
+print(len(name))
+# var_name -> Identifier
+# var_val -> Literals
+# Literals are the actual values assigned
+# Literals can be Numeric and Non-Numeric.
+age = 12
+print(age)
+print(type(age))
+#print(len(age)) TypeError: object of type 'int' has no len()
+pi = 3.14
+print(pi)
+print(type(pi))
+is_married = True
+print(is_married)
+print(type(is_married))
+have_lambo = None
+print(have_lambo)
+print(type(have_lambo))
+my_list = []
+print(my_list)
+print(len(my_list))
+my_set = {}
+print(my_set)
+print(len(my_set))
---
+++ Ex_10062024/Lab034.py
@@ -0,0 +1,13 @@
+# Ternary Operator
+barsha_marks = 90
+utpal_marks = 97
+
+#   x > y -> do something - print("pramod")
+# y > x -> do something else -> print("amit)
+print("barsha is winner" if barsha_marks > utpal_marks else "utpal is winner")
+
+
+if barsha_marks > utpal_marks:
+    print("barsha is the winner")
+else:
+    print("utpal is the winner")
